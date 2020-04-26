08609525d707130d7368c289a073a34c48ce4a13
François Freitag <mail@franek.fr>
2020-04-26 19:43:41 +0200

    sqlflush

--- django/core/management/commands/sqlflush.py
+++ django/core/management/commands/sqlflush.py
@@ -21,5 +21,5 @@ class Command(BaseCommand):
     def handle(self, **options):
         sql_statements = sql_flush(self.style, connections[options['database']])
         if not sql_statements and options['verbosity'] >= 1:
-            self.stderr.write('No tables found.')
+            self.logger.error('No tables found.')
         return '\n'.join(sql_statements)
--- tests/empty_models/test_commands.py
+++ tests/empty_models/test_commands.py
@@ -8,11 +8,11 @@ class CoreCommandsNoOutputTests(TestCase):
     available_apps = ['empty_models']
 
     def test_sqlflush_no_tables(self):
-        out = io.StringIO()
-        err = io.StringIO()
-        call_command('sqlflush', stdout=out, stderr=err)
-        self.assertEqual(out.getvalue(), '')
-        self.assertEqual(err.getvalue(), 'No tables found.\n')
+        with self.assertLogs('django.command', 'ERROR') as logs:
+            call_command('sqlflush')
+        [log] = logs.records
+        self.assertEqual(log.levelname, 'ERROR')
+        self.assertEqual(log.msg, 'No tables found.')
 
     def test_sqlsequencereset_no_sequences(self):
         out = io.StringIO()

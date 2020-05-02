d8a7cdc00bc8feab70f0a81fd73ebcce74b380d8
François Freitag <mail@franek.fr>
2020-05-02 14:07:46 +0200

    Port reverse_url from user commands tests

--- tests/user_commands/management/commands/reverse_url.py
+++ tests/user_commands/management/commands/reverse_url.py
@@ -7,4 +7,4 @@ class Command(BaseCommand):
     This command returns a URL from a reverse() call.
     """
     def handle(self, *args, **options):
-        return reverse('some_url')
+        return (reverse('some_url'),)
